a0ede55199c9e62001649a588cd0c87a669e346d
Bohdan Zlotko <bohdanzlotko@gmail.com>
2023-06-25 23:28:56 +0300

    fix

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -2,7 +2,7 @@ const theme = Object.freeze({
   colors: {
     primary: '#fff',
     primaryBg: '#ededed',
-    secondaryBg: 'rgb(113, 181, 249)',
+    secondaryBg: 'rgb(113, 81, 249)',
     primaryText: '#212121',
     secondaryText: '#757575',
   },
